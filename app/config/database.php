12079587a04afd43711f0bd8f85dbf7fabc4502a
mq.Jose <mq.josedavid@gmail.com>
2014-06-16 16:47:17 -0400

    david

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'laravel',
 			'username'  => 'root',
-			'password'  => 'sam',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
